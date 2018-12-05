00116f2ad8958973440f1cbba3a50ac33aca192d
Elphabecca <becca@streak.com>
2018-12-05 11:29:33 -0800

    day 5 skel

---
+++ aoc_day5.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('aoc_day5_data.txt', 'utf8');
+
+const testData = "dabAcCaCBAcCcaDA";
+// PART A
+// Get rid of the reactants (Ee or eE) in the data
+// return the length of the final string (testData = 10, final string for testData is dabCBAcaDA)
+
+console.log("!!! PART A !!!")
+
+// console.log(data)
+// console.log(testData)
+
+// PART B
+
+// console.log("!!! PART B !!!")
