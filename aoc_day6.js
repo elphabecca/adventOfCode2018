ced24d987dabf505904305b772ba893c425a8d89
Elphabecca <becca@streak.com>
2018-12-05 14:12:04 -0800

    day 6 skel

---
+++ aoc_day6.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('aoc_day6_data.txt', 'utf8').split("\n");
+
+const testData = ;
+
+// PART A
+
+console.log("\n\n!!! PART A !!!\n\n")
+
+
+// PART B
+
+// console.log("\n\n!!! PART B !!!\n\n")
