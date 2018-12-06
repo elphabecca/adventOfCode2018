5ec5e5ad93eaba3d0e0373b413e6084bd65640d9
Elphabecca <becca@streak.com>
2018-12-06 11:33:20 -0800

    day 6 data add

--- aoc_day6.js
+++ aoc_day6.js
@@ -2,11 +2,15 @@ const fs = require('fs');
 
 const data = fs.readFileSync('aoc_day6_data.txt', 'utf8').split("\n");
 
-const testData = ;
+let testData = "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9";
+testData = testData.split("\n");
 
 // PART A
 
 console.log("\n\n!!! PART A !!!\n\n")
+
+// console.log(data);
+console.log(testData);
 
 
 // PART B
